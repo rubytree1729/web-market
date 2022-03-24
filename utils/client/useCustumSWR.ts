820a6ad8776da3916912c99e3dc986d55f6f3461
lemontree1729 <songyj927@gmail.com>
2022-03-24 14:10:19 +0900

    modify customswr

--- utils/client/useCustumSWR.ts
+++ utils/client/useCustumSWR.ts
@@ -4,13 +4,12 @@ import customAxios from "../customAxios"
 
 export default function useCustomSWR(url: string, config?: AxiosRequestConfig) {
     const fetcher = (url: string) => customAxios(url, config).then(res => res.data)
-    let { data, error } = useSWR(url, fetcher)
-    if (data.error) {
-        error = data.error
-    }
+    const { data, error } = useSWR(url, fetcher)
+    const finaldata = data ? data.result : data
+    const finalerror = (data && data.error) ? data.error : error
     return {
-        data: data.result,
-        isLoading: !error && !data,
-        isError: error
+        data: finaldata,
+        isLoading: (error === undefined) && (data === undefined),
+        isError: finalerror
     }
 }
