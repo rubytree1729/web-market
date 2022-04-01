3d6889d98a81a1fe0fa3882ebc30785ec96369cb
akeias <akeias7193@naver.com>
2022-04-01 15:51:41 +0900

    0401

--- component/index/categoryList.tsx
+++ component/index/categoryList.tsx
@@ -7,14 +7,14 @@ function categoryList(props: any) {
             <div className={styles.container}>
                 <img className={styles.image} src={props.imageUrl}></img>
                 <div className={styles.itemInfo}>
-                    <div>{props.name}</div>
-                    <div>{props.price}</div>
-                    <div>{props.category1} /{props.category2}</div>
-                    <div>찜하기 등 기타 여러개?</div>
+                    <div className={styles.infoStyle}>{props.name}</div>
+                    <div className={styles.infoStyle}>{props.price}</div>
+                    <div className={styles.infoStyle}>{props.category1} | {props.category2}</div>
+                    <div className={styles.infoStyle}>찜하기 등 기타 여러개?</div>
                 </div>
                 <div className={styles.sideInfo}>
-                    <div>{props.maker}</div>
-                    <div>택배비    3500원</div>
+                    <div className={styles.makerStyle}>{props.maker}</div>
+                    <div className={styles.postStyle}>택배비    3500원</div>
                 </div>
             </div>
         </div>
