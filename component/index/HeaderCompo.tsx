2951c14495b737a3301cc8a20847063ed7742c84
ekdlwp123 <ekdlwp123@gamil.com>
2022-04-06 19:09:52 +0900

    heder css

--- component/index/HeaderCompo.tsx
+++ component/index/HeaderCompo.tsx
@@ -12,30 +12,51 @@ const HeaderCompo: NextPage = () => {
     }
     if (isApiError) {
         return (
-            <div>
-                <div className={styles.header}>
-                    <div className={styles.headbar}>
-                        <div className={styles.category}>
-                            <MenuToggle />
-                        </div>
-                        <div className={styles.searchcontainer}>
-                            <Link href="/" passHref>
-                                <div className={styles.logo}></div>
-                            </Link>
+            <div className={styles.header}>
+                <div className={styles.headbar}>
 
-                            <div className={styles.search}>
-                                <input type="text" />
-                                <button></button>
-                            </div>
-                        </div>
-                        <div className={styles.mypage}>
-                            <Link href="/login" passHref>
-                                <div className={styles.loginBtn}>로그인</div>
-                            </Link>
+                    <div className={styles.category}>
+                        <MenuToggle />
+
+                        <Link href="/" passHref>
+                            <div className={styles.logo}></div>
+                        </Link>
+                    </div>
+
+                    <div className={styles.searchcontainer}>
+                        <div className={styles.search}>
+                            <input type="text" />
+                            <button></button>
                         </div>
                     </div>
+
+                    <div className={styles.logingroup}>
+
+                        <Link href="/login" passHref>
+                            <div className={styles.loginBtn}>로그인</div>
+                        </Link>
+                        <span>|</span>
+
+                        <Link href="/signup" passHref>
+                            <div className={styles.loginBtn}>회원가입</div>
+                        </Link>
+
+                    </div>
+                    <div className={styles.mypage}>
+                        <Link href="/mypage" passHref>
+                            <div className={styles.mypagebtn}>
+                            </div>
+                        </Link>
+                        <div className={styles.itemBox}>
+                        </div>
+
+                    </div>
                 </div>
+
             </div >
+
+
+
         )
     }
     const { role } = data
@@ -44,34 +65,41 @@ const HeaderCompo: NextPage = () => {
             <div>
                 <div className={styles.header}>
                     <div className={styles.headbar}>
+
                         <div className={styles.category}>
                             <MenuToggle />
-                        </div>
-                        <div className={styles.searchcontainer}>
+
                             <Link href="/" passHref>
                                 <div className={styles.logo}></div>
                             </Link>
+                        </div>
 
+                        <div className={styles.searchcontainer}>
                             <div className={styles.search}>
                                 <input type="text" />
                                 <button></button>
                             </div>
                         </div>
-                        <div className={styles.mypage}>
+
+                        <div className={styles.logingroup}>
                             <Link href="/admin" passHref>
                                 <div>
                                     관리자
                                 </div>
                             </Link>
-                            <div className={styles.itemBox}>
-                            </div>
+                            <span>|</span>
+                            <Link href="/api/logout" passHref>
+                                <div className={styles.loginbtn}>로그아웃</div>
+                            </Link>
+                        </div>
+                        <div className={styles.mypage}>
                             <Link href="/mypage" passHref>
                                 <div className={styles.mypagebtn}>
                                 </div>
                             </Link>
-                            <Link href="/api/logout" passHref>
-                                <div className={styles.loginbtn}>로그아웃</div>
-                            </Link>
+                            <div className={styles.itemBox}>
+                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -82,29 +110,35 @@ const HeaderCompo: NextPage = () => {
             <div>
                 <div className={styles.header}>
                     <div className={styles.headbar}>
+
                         <div className={styles.category}>
                             <MenuToggle />
-                        </div>
-                        <div className={styles.searchcontainer}>
+
                             <Link href="/" passHref>
                                 <div className={styles.logo}></div>
                             </Link>
+                        </div>
 
+                        <div className={styles.searchcontainer}>
                             <div className={styles.search}>
                                 <input type="text" />
                                 <button></button>
                             </div>
                         </div>
+
+                        <div className={styles.logingroup}>
+                            <Link href="/api/logout" passHref>
+                                <div className={styles.loginbtn}>로그아웃</div>
+                            </Link>
+                        </div>
                         <div className={styles.mypage}>
-                            <div className={styles.itemBox}>
-                            </div>
                             <Link href="/mypage" passHref>
                                 <div className={styles.mypagebtn}>
                                 </div>
                             </Link>
-                            <Link href="/api/logout" passHref>
-                                <div className={styles.loginbtn}>로그아웃</div>
-                            </Link>
+                            <div className={styles.itemBox}>
+                            </div>
+
                         </div>
                     </div>
                 </div>
